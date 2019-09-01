2162e39d4552797342820d5799363c55aea4e262
root <root@localhost.localdomain>
2019-09-01 21:41:02 +0400

    for public: batch-file-rename-util

---
+++ src/main/java/my/harp07/NewMain.java
@@ -0,0 +1,20 @@
+
+package my.harp07;
+
+import java.io.File;
+
+public class NewMain {
+
+    public static void main(String[] args) {
+        String [] dirlist=new File("rename").list();
+        System.out.println(dirlist);
+        int j=10;
+        for (String name: dirlist) {
+            File ff=new File("rename/" + name);
+            ff.renameTo(new File("rename/" + j + ".jar"));
+            System.out.println("rename/" + name + " to rename/" + j + ".jar");
+            j++;
+        }
+    }
+    
+}
